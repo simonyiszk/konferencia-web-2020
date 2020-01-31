abbb183bed2aed8ee53fa1c22e6cf7299f4cdf20
AndrisBorbas <andris.borbas@gmail.com>
2020-01-31 16:29:46 +0100

    Add sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Image } from '@chakra-ui/core';
-import React from 'react';
+import React, { Component } from 'react';
+import Sticky from 'react-sticky-el';
 
 import HeroBG from '../assets/star_PNG76860.png';
 import customTheme from '../gatsby-plugin-chakra-ui/theme';
@@ -8,24 +9,28 @@ export default function Header(): JSX.Element {
   return (
     <Box m={0} p={0} h="100vh">
       <Image src={HeroBG} w="100vw" maxW="100%" />
+
       <Box
-        m="auto"
-        p={4}
-        bg="gray.900"
+        m={0}
+        p={0}
         position="absolute"
         bottom="0px"
         w="100vw"
         maxW="100%"
         zIndex="sticky"
       >
-        <Flex flexDirection="row" maxWidth="66vw" align="center" m="auto">
-          <Heading as="h3" m="auto">
-            Előadók
-          </Heading>
-          <Heading as="h3" m="auto">
-            Szervezők
-          </Heading>
-        </Flex>
+        <Sticky>
+          <Box m="auto" p={4} bg="gray.900">
+            <Flex flexDirection="row" maxWidth="66vw" align="center" m="auto">
+              <Heading as="h3" m="auto">
+                Előadók
+              </Heading>
+              <Heading as="h3" m="auto">
+                Szervezők
+              </Heading>
+            </Flex>
+          </Box>
+        </Sticky>
       </Box>
     </Box>
   );
