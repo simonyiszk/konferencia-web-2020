828152581e9b5b25c50d0f6e2dbdb387df1237a3
AndrisBorbas <andris.borbas@gmail.com>
2020-01-31 21:17:24 +0100

    fix parallax effect ??

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,20 @@
 import { Box, Flex, Heading, Image } from '@chakra-ui/core';
 import React, { Component } from 'react';
-import {
-  Parallax,
-  ParallaxBanner,
-  ParallaxProvider,
-} from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
 import Sticky from 'react-sticky-el';
 
 import HeroBG from '../assets/star_PNG76860.png';
 import customTheme from '../gatsby-plugin-chakra-ui/theme';
+import ParallaxCache from './ParallaxCache';
 
 export default function Header(): JSX.Element {
   return (
     <Box m={0} p={0} h="100vh">
-      <ParallaxProvider>
-        <Parallax className="custom-class" y={[-50, 50]} tagOuter="figure">
-          <Image src={HeroBG} /* w="100vw" maxW="100%" */ />
-        </Parallax>
-        {/*
-        <ParallaxBanner
-          className="your-class"
-          layers={[
-            {
-              image: 'https://foo.com/foo.jpg',
-              amount: 0.1,
-            },
-            {
-              image: 'https://foo.com/bar.png',
-              amount: 0.2,
-            },
-          ]}
-          style={{
-            height: '500px',
-          }}
-        >
-          <h1>Banner Children</h1>
-        </ParallaxBanner>
-        */}
-      </ParallaxProvider>
+      <ParallaxCache />
+      <Parallax className="custom-class" y={[-50, 100]} tagOuter="figure">
+        <Image src={HeroBG} /* w="100vw" maxW="100%" */ />
+      </Parallax>
+
       <Box
         m={0}
         p={0}
