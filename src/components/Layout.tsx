6642a6d06be237bec9f9b47be0d9f4c8666057f3
Andris Borbas <andris.borbas@gmail.com>
2020-02-25 03:42:48 +0100

    fix ssr

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -44,6 +44,20 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
     return null;
   }
 
+  if (typeof window !== 'undefined') {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
   return (
     <React.StrictMode>
       <Helmet
