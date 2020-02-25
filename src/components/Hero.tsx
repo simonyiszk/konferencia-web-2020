658025f21a36ad1f111ab379b18d3f6047683898
Andris Borbas <andris.borbas@gmail.com>
2020-02-25 02:31:04 +0100

    test mobile navbar placement 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,10 @@ import { Parallax, ParallaxContext } from 'react-scroll-parallax';
 import Sticky from 'react-sticky-el';
 
 import HeroBG from '../assets/star_PNG76860.png';
+import HeroLayer1 from '../assets/top-layer-1.png';
+import HeroLayer2 from '../assets/top-layer-2.png';
+import HeroLayer4 from '../assets/top-layer-4.png';
+import HeroLayer6 from '../assets/top-layer-6.png';
 import {
   Parallax3DGroup,
   Parallax3DLayerBack,
@@ -13,35 +17,8 @@ import {
 
 export default function Hero(): JSX.Element {
   return (
-    <Box as="header" m={0} p={0} h="calc(100vh - 78px)">
+    <Box as="header" m={0} p={0} h="calc(100% - 78px)">
       <Parallax3DGroup>
-        <Parallax3DLayerBack>
-          <Box
-            css={{ textAlign: 'center' }}
-            position="absolute"
-            left="50%"
-            top="50%"
-            transform="translate(-50%, -50%)"
-          >
-            <Image src={HeroBG} w="100vw" maxW="unset" />
-          </Box>
-        </Parallax3DLayerBack>
-        <Parallax3DLayerFore>
-          <Box
-            css={{ textAlign: 'center' }}
-            position="absolute"
-            left="50%"
-            top="50%"
-            transform="translate(-50%, -50%)"
-          >
-            <Image
-              src={HeroBG}
-              w="150vw"
-              maxW="unset"
-              transform="translate(-50px, -50px)"
-            />
-          </Box>
-        </Parallax3DLayerFore>
         <Parallax3DLayerDeep>
           <Box
             css={{ textAlign: 'center' }}
@@ -51,13 +28,41 @@ export default function Hero(): JSX.Element {
             transform="translate(-50%, -50%)"
           >
             <Image
-              src={HeroBG}
-              w="100vw"
+              src={HeroLayer6}
+              w="130vw"
               maxW="unset"
               transform="translate(50px, 50px)"
             />
           </Box>
         </Parallax3DLayerDeep>
+        <Parallax3DLayerBack>
+          <Box
+            css={{ textAlign: 'center' }}
+            position="absolute"
+            left="50%"
+            top="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Image src={HeroLayer4} w="100vw" maxW="unset" />
+          </Box>
+        </Parallax3DLayerBack>
+
+        <Parallax3DLayerFore>
+          <Box
+            css={{ textAlign: 'center' }}
+            position="absolute"
+            left="50%"
+            top="50%"
+            transform="translate(-50%, -50%)"
+          >
+            <Image
+              src={HeroLayer2}
+              w="130vw"
+              maxW="unset"
+              transform="translate(-50px, -50px)"
+            />
+          </Box>
+        </Parallax3DLayerFore>
       </Parallax3DGroup>
     </Box>
   );
